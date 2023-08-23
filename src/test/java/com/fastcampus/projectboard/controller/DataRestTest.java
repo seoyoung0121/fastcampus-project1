d3989c48349e7938e7d73151551f22ba61b8d695
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2023-08-23 13:09:38 +0900

    #1 - 테스트 제외 처리

--- src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@Disabled("불필요하므로 제외")
 @DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
