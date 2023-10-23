d2da77270a43d07549d748404e8c70c0f73f5f64
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2023-10-24 00:03:39 +0200

    #36 - 게시글 댓글 구현

--- src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
@@ -3,6 +3,7 @@ package com.fastcampus.projectboard.dto;
 import java.time.LocalDateTime;
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.ArticleComment;
+import com.fastcampus.projectboard.domain.UserAccount;
 
 public record ArticleCommentDto(
         Long id,
@@ -17,6 +18,9 @@ public record ArticleCommentDto(
     public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
         return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
     }
+    public static ArticleCommentDto of(Long articleId, UserAccountDto userAccountDto, String content){
+        return  new ArticleCommentDto(null, articleId, userAccountDto, content, null, null, null, null);
+    }
     public static ArticleCommentDto from(ArticleComment entity) {
         return new ArticleCommentDto(
                 entity.getId(),
@@ -29,10 +33,10 @@ public record ArticleCommentDto(
                 entity.getModifiedBy()
         );
     }
-    public ArticleComment toEntity(Article entity) {
+    public ArticleComment toEntity(Article article, UserAccount userAccount) {
         return ArticleComment.of(
-                entity,
-                userAccountDto.toEntity(),
+                article,
+                userAccount,
                 content
         );
     }
